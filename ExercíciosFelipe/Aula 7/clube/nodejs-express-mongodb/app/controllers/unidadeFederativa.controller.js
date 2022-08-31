21d9ded77d9075fcde2939c98399b7bde032ae76
Felipe Nicaretta <felipe@evolusoft.local>
2022-08-31 13:36:53 -0300

    aula 7 atualiza

--- ExercíciosFelipe/Aula 7/clube/nodejs-express-mongodb/app/controllers/unidadeFederativa.controller.js
+++ ExercíciosFelipe/Aula 7/clube/nodejs-express-mongodb/app/controllers/unidadeFederativa.controller.js
@@ -1,16 +1,6 @@
-const db = require("../models");
-const UnidadeFederativa = db.estados;
-
-//Cria e salva um novo documento para nova entidade
-exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.sigla) {
-        res.status(400).send({ message: "Conteudo nao pode ser vazio!" });
-        return;
-}
-   // Create a UnidadeFederativa
-   const unidadeFederativa = new UnidadeFederativa ({
-      sigla: req.body.sigla ? req.body.sigla : null  ,
-      nome: req.body.nome ? req.body.nome : null  
-   });
-};
+EMI2005: Failed to compile generator runtime 
+ Errors were reported during stylesheet compilation
+          <xsl:value-of select=" ancestors:class/@name "/>
+  EMI5002: Failed to compile with message 
+     Namespace prefix 'ancestors' has not been declared
+     ; SystemID: ; Line#: 162; Column#: 16
