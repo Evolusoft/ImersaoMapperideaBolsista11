a2233246421b3a4454f4557a13041898ab115c5e
Felipe Nicaretta <felipe@evolusoft.local>
2022-09-05 13:47:46 -0300

    aula 8

--- ExercíciosFelipe/Aula 8/clube/nodejs-express-mongodb/app/controllers/bairro.controller.js
+++ ExercíciosFelipe/Aula 8/clube/nodejs-express-mongodb/app/controllers/bairro.controller.js
@@ -1,45 +1,45 @@
 const db = require("../models");
 const Bairro = db.bairros;
 validaCamposRequeridosBairro = (req) => {
-const CamposRequeridosEmpty = new Array();
-if (!req.body.cidade) {
-camposRequeridosEmpty.push(" cidade");
-}
-if (!req.body.nome) {
-camposRequeridosEmpty.push(" nome");
-}
-if (!req.body.cepPadrao) {
-camposRequeridosEmpty.push(" cepPadrao");
-}
-return CamposRequeridosEmpty;
+   const CamposRequeridosEmpty = new Array();
+   if (!req.body.cidade) {
+      camposRequeridosEmpty.push(" cidade");
+   }
+   if (!req.body.nome) {
+      camposRequeridosEmpty.push(" nome");
+   }
+   if (!req.body.cepPadrao) {
+      camposRequeridosEmpty.push(" cepPadrao");
+   }
+   return CamposRequeridosEmpty;
 }
 
 //Cria e salva um novo documento para nova entidade
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.cidade) {
-        res.status(400).send({ message: "Conteudo nao pode ser vazio!" });
-        return;
-}
-const camposRequeridosEmpty = validaCamposRequeridos.Bairro (req);
-if (camposRequeridosEmpty.length > 0) {
-res.status(400).send({message: "campos requeridos ("+camposRequeridosEmpty.join (",") + "nao podem ser vazios" });
-return;
-}
+   // Validate request
+   if (!req.body.cidade) {
+      res.status(400).send({ message: "Conteudo nao pode ser vazio!" });
+      return;
+   }
+   const camposRequeridosEmpty = validaCamposRequeridos.Bairro(req);
+   if (camposRequeridosEmpty.length > 0) {
+      res.status(400).send({ message: "campos requeridos (" + camposRequeridosEmpty.join(",") + "nao podem ser vazios" });
+      return;
+   }
    // Create a Bairro
-   const bairro = new Bairro ({
-      nome: req.body.nome ? req.body.nome : null ,
-      cepPadrao: req.body.cepPadrao ? req.body.cepPadrao : 0  
+   const bairro = new Bairro({
+      nome: req.body.nome ? req.body.nome : null,
+      cepPadrao: req.body.cepPadrao ? req.body.cepPadrao : 0
    });
    bairro
       .save(bairro)
       .then(data => {
          res.send(data);
-   })
-      .catch(err => { 
+      })
+      .catch(err => {
          res.status(500).send({
-         message:
-         err.message || "Ocorreu um erro de servidor ao tentar salvar Bairro." 
-      }); 
-   }); 
+            message:
+               err.message || "Ocorreu um erro de servidor ao tentar salvar Bairro."
+         });
+      });
 };
--- ExercíciosFelipe/Aula 8/clube/nodejs-express-mongodb/app/controllers/cidade.controller.js
+++ ExercíciosFelipe/Aula 8/clube/nodejs-express-mongodb/app/controllers/cidade.controller.js
@@ -1,48 +1,48 @@
 const db = require("../models");
 const Cidade = db.cidades;
 validaCamposRequeridosCidade = (req) => {
-const CamposRequeridosEmpty = new Array();
-if (!req.body.codigo) {
-camposRequeridosEmpty.push(" codigo");
-}
-if (!req.body.nome) {
-camposRequeridosEmpty.push(" nome");
-}
-if (!req.body.uf) {
-camposRequeridosEmpty.push(" uf");
-}
-if (!req.body.bairros) {
-camposRequeridosEmpty.push(" bairros");
-}
-return CamposRequeridosEmpty;
+   const CamposRequeridosEmpty = new Array();
+   if (!req.body.codigo) {
+      camposRequeridosEmpty.push(" codigo");
+   }
+   if (!req.body.nome) {
+      camposRequeridosEmpty.push(" nome");
+   }
+   if (!req.body.uf) {
+      camposRequeridosEmpty.push(" uf");
+   }
+   if (!req.body.bairros) {
+      camposRequeridosEmpty.push(" bairros");
+   }
+   return CamposRequeridosEmpty;
 }
 
 //Cria e salva um novo documento para nova entidade
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.codigo) {
-        res.status(400).send({ message: "Conteudo nao pode ser vazio!" });
-        return;
-}
-const camposRequeridosEmpty = validaCamposRequeridos.Cidade (req);
-if (camposRequeridosEmpty.length > 0) {
-res.status(400).send({message: "campos requeridos ("+camposRequeridosEmpty.join (",") + "nao podem ser vazios" });
-return;
-}
+   // Validate request
+   if (!req.body.codigo) {
+      res.status(400).send({ message: "Conteudo nao pode ser vazio!" });
+      return;
+   }
+   const camposRequeridosEmpty = validaCamposRequeridos.Cidade(req);
+   if (camposRequeridosEmpty.length > 0) {
+      res.status(400).send({ message: "campos requeridos (" + camposRequeridosEmpty.join(",") + "nao podem ser vazios" });
+      return;
+   }
    // Create a Cidade
-   const cidade = new Cidade ({
-      codigo: req.body.codigo ? req.body.codigo : 0 ,
-      nome: req.body.nome ? req.body.nome : null  
+   const cidade = new Cidade({
+      codigo: req.body.codigo ? req.body.codigo : 0,
+      nome: req.body.nome ? req.body.nome : null
    });
    cidade
       .save(cidade)
       .then(data => {
          res.send(data);
-   })
-      .catch(err => { 
+      })
+      .catch(err => {
          res.status(500).send({
-         message:
-         err.message || "Ocorreu um erro de servidor ao tentar salvar Cidade." 
-      }); 
-   }); 
+            message:
+               err.message || "Ocorreu um erro de servidor ao tentar salvar Cidade."
+         });
+      });
 };
