eabe3eb9f79f24492db77ee2f360f36f4af05e20
Felipe Nicaretta <felipe@evolusoft.local>
2022-10-15 10:14:46 -0300

    arruma addpais

--- Fontes/struct/frontEnd/src/app/components/add-pais/add-pais.component.ts
+++ Fontes/struct/frontEnd/src/app/components/add-pais/add-pais.component.ts
@@ -3,38 +3,39 @@ import { Pais } from 'src/app/models/pais.model';
 import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
-selector: 'app-add-pais ',
-templateUrl: './add-pais .component.html',
-styleUrls: ['./add-pais .component.css']
+  selector: 'app-add-pais ',
+  templateUrl: './add-pais.component.html',
+  styleUrls: ['./add-pais.component.css']
 })
 export class AddPaisComponent implements OnInit {
+
   pais: Pais = {
-    sigla:'',
-    codigo:0,
-    nome:'',
-    ativo:
+    sigla: '',
+    codigo: 0,
+    nome: '',
+    ativo: false
   };
   submitted = false;
   debug = true;
 
-
-  constructor(private paisService:PaisService) { }
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
-   }
-}
+  }
 
-   savePais(): void {
+
+  savePais(): void {
+    const data = {
       codigo: this.pais.codigo,
       sigla: this.pais.sigla,
       nome: this.pais.nome,
-      ativo: this.pais.ativo,
-};
+      ativo: this.pais.ativo
+    };
 
     this.paisService.create(data)
       .subscribe(
         response => {
-          if (debug) console.log(response); 
+          if (this.debug) console.log(response);
           this.submitted = true;
         },
         error => {
@@ -45,12 +46,11 @@ export class AddPaisComponent implements OnInit {
   newPais(): void {
     this.submitted = false;
     this.debug = true;
-
     this.pais = {
-    sigla:,
-    codigo:,
-    nome:,
-    ativo:
+      sigla: '',
+      codigo: 0,
+      nome: '',
+      ativo: false
     };
   }
 }
